3d6a86b1d98436e2036b8861cd98745ef5c79fdb
joshuaimbusi <joshua.imbusi@student.moringaschool.com>
2025-10-15 19:53:36 +0300

    "initial commit"

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart] = useState(false);
+
+  function handleAddToCartClick() {
+    setInCart((prevInCart) => !prevInCart);
+  }
+
+  const buttonText = inCart ? "Remove From Cart" : "Add to Cart";
+  const liClass = inCart ? "in-cart" : "";
+  const buttonClass = inCart ? "remove" : "add";
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={handleAddToCartClick}>
+        {buttonText}
+      </button>
     </li>
   );
 }
 
 export default Item;
+
